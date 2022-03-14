dc3f8c80615835670801f5745ff0500c7bfcd1df
Mikhail Savelov <savelov.m@gmail.com>
2022-03-15 02:45:54 +0300

    report

---
+++ params_da.py
@@ -0,0 +1,12 @@
+	num_agent_train_steps_per_iter = 1000 
+
+ 	batch_size = 1000
+ 	eval_batch_size =  10000
+ 	train_batch_size =  100
+	max_replay_buffer_size = 1000000
+
+	n_layers =  2
+	size =  64
+	learning_rate =  5e-3
+	
+	seed = 0
