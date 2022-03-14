1dee86c8d44c44108b9f175c3c816a540f7afec1
setra2014 <36576562+setra2014@users.noreply.github.com>
2022-03-14 03:12:28 +0300

    Add files via upload

--- hw1/cds_rl/policies/MLP_policy.py
+++ hw1/cds_rl/policies/MLP_policy.py
@@ -79,9 +79,9 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs
         else:
             observation = obs[None]
-
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        action = self(torch.Tensor(observation).to(ptu.device))
+        return action.cpu().detach().numpy()
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +93,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        if self.discrete:
+            return self.logits_na(observation)
+        else:
+            return self.mean_net(observation)
 
 
 #####################################################
@@ -109,7 +112,10 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        self.optimizer.zero_grad()
+        loss = self.loss(self(torch.Tensor(observations).to(ptu.device)), torch.Tensor(actions).to(ptu.device))
+        loss.backward()
+        self.optimizer.step()
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
