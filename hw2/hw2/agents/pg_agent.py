284979dc8914d0b7b688f463356212549dc2287f
Yuliya smolkina <smolkina.djulia@gmail.com>
2022-04-21 12:40:57 +0300

    Updated agent.py

--- hw2/hw2/agents/pg_agent.py
+++ hw2/hw2/agents/pg_agent.py
@@ -1,177 +1,247 @@
+import time
+from collections import OrderedDict
+
+import gym
 import numpy as np
+import torch
+import pickle
 
-from hw2.agents.base_agent import BaseAgent
-from hw2.policies.MLP_policy import MLPPolicyPG
-from hw2.infrastructure.replay_buffer import ReplayBuffer
+from hw2.infrastructure import pytorch_util as ptu
+from hw2.infrastructure import utils
+from hw2.infrastructure.action_noise_wrapper import ActionNoiseWrapper
+from hw2.infrastructure.logger import Logger
+
+# how many rollouts to save as videos to tensorboard
+MAX_NVIDEO = 2
+MAX_VIDEO_LEN = 40 # we overwrite this in the code below
 
 
-class PGAgent(BaseAgent):
-    def __init__(self, env, agent_params):
-        super(PGAgent, self).__init__()
+class RL_Trainer(object):
 
-        # init vars
-        self.env = env
-        self.agent_params = agent_params
-        self.gamma = self.agent_params['gamma']
-        self.standardize_advantages = self.agent_params['standardize_advantages']
-        self.nn_baseline = self.agent_params['nn_baseline']
-        self.reward_to_go = self.agent_params['reward_to_go']
-        self.gae_lambda = self.agent_params['gae_lambda']
+    def __init__(self, params):
 
-        # actor/policy
-        self.actor = MLPPolicyPG(
-            self.agent_params['ac_dim'],
-            self.agent_params['ob_dim'],
-            self.agent_params['n_layers'],
-            self.agent_params['size'],
-            discrete=self.agent_params['discrete'],
-            learning_rate=self.agent_params['learning_rate'],
-            nn_baseline=self.agent_params['nn_baseline']
+        #############
+        ## INIT
+        #############
+
+        # Get params, create logger
+        self.params = params
+        self.logger = Logger(self.params['logdir'])
+
+        # Set random seeds
+        seed = self.params['seed']
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        ptu.init_gpu(
+            use_gpu=not self.params['no_gpu'],
+            gpu_id=self.params['which_gpu']
         )
 
-        # replay buffer
-        self.replay_buffer = ReplayBuffer(1000000)
+        #############
+        ## ENV
+        #############
 
-    def train(self, observations, actions, rewards_list, next_observations, terminals):
+        # Make the gym environment
+        self.env = gym.make(self.params['env_name'])
+        self.env.seed(seed)
 
-        """
-            Training a PG agent refers to updating its actor using the given observations/actions
-            and the calculated qvals/advantages that come from the seen rewards.
-        """
+        # Add noise wrapper
+        if params['action_noise_std'] > 0:
+            self.env = ActionNoiseWrapper(self.env, seed, params['action_noise_std'])
 
-        # TODO: update the PG actor/policy using the given batch of data, and
-        # return the train_log obtained from updating the policy
+        # import plotting (locally if 'obstacles' env)
+        if not(self.params['env_name']=='obstacles-cs285-v0'):
+            import matplotlib
+            matplotlib.use('Agg')
 
-        # HINT1: use helper functions to compute qvals and advantages
-        # HINT2: look at the MLPPolicyPG class for how to update the policy
-            # and obtain a train_log
+        # Maximum length for episodes
+        self.params['ep_len'] = self.params['ep_len'] or self.env.spec.max_episode_steps
+        global MAX_VIDEO_LEN
+        MAX_VIDEO_LEN = self.params['ep_len']
 
-        return train_log
+        # Is this env continuous, or self.discrete?
+        discrete = isinstance(self.env.action_space, gym.spaces.Discrete)
+        # Are the observations images?
+        img = len(self.env.observation_space.shape) > 2
 
-    def calculate_q_vals(self, rewards_list):
+        self.params['agent_params']['discrete'] = discrete
 
-        """
-            Monte Carlo estimation of the Q function.
-        """
+        # Observation and action sizes
 
-        # TODO: return the estimated qvals based on the given rewards, using
-            # either the full trajectory-based estimator or the reward-to-go
-            # estimator
+        ob_dim = self.env.observation_space.shape if img else self.env.observation_space.shape[0]
+        ac_dim = self.env.action_space.n if discrete else self.env.action_space.shape[0]
+        self.params['agent_params']['ac_dim'] = ac_dim
+        self.params['agent_params']['ob_dim'] = ob_dim
 
-        # HINT1: rewards_list is a list of lists of rewards. Each inner list
-            # is a list of rewards for a single trajectory.
-        # HINT2: use the helper functions self._discounted_return and
-            # self._discounted_cumsum (you will need to implement these). These
-            # functions should only take in a single list for a single trajectory.
-
-        # Case 1: trajectory-based PG
-        # Estimate Q^{pi}(s_t, a_t) by the total discounted reward summed over entire trajectory
-        # HINT3: q_values should be a 1D numpy array where the indices correspond to the same
-        # ordering as observations, actions, etc.
-
-        if not self.reward_to_go:
-            TODO
-
-        # Case 2: reward-to-go PG
-        # Estimate Q^{pi}(s_t, a_t) by the discounted sum of rewards starting from t
+        # simulation timestep, will be used for video saving
+        if 'model' in dir(self.env):
+            self.fps = 1/self.env.model.opt.timestep
+        elif 'env_wrappers' in self.params:
+            self.fps = 30 # This is not actually used when using the Monitor wrapper
+        elif 'video.frames_per_second' in self.env.env.metadata.keys():
+            self.fps = self.env.env.metadata['video.frames_per_second']
         else:
-            TODO
+            self.fps = 10
 
-        return q_values
 
-    def estimate_advantage(self, obs, rews_list, q_values, terminals):
+        #############
+        ## AGENT
+        #############
 
+        agent_class = self.params['agent_class']
+        self.agent = agent_class(self.env, self.params['agent_params'])
+
+    def run_training_loop(self, n_iter, collect_policy, eval_policy,
+                          initial_expertdata=None, relabel_with_expert=False,
+                          start_relabel_with_expert=1, expert_policy=None):
         """
-            Computes advantages by (possibly) using GAE, or subtracting a baseline from the estimated Q values
+        :param n_iter:  number of (dagger) iterations
+        :param collect_policy:
+        :param eval_policy:
+        :param initial_expertdata:
+        :param relabel_with_expert:  whether to perform dagger
+        :param start_relabel_with_expert: iteration at which to start relabel with expert
+        :param expert_policy:
         """
 
-        # Estimate the advantage when nn_baseline is True,
-        # by querying the neural network that you're using to learn the value function
-        if self.nn_baseline:
-            values_unnormalized = self.actor.run_baseline_prediction(obs)
-            ## ensure that the value predictions and q_values have the same dimensionality
-            ## to prevent silent broadcasting errors
-            assert values_unnormalized.ndim == q_values.ndim
-            ## TODO: values were trained with standardized q_values, so ensure
-                ## that the predictions have the same mean and standard deviation as
-                ## the current batch of q_values
-            values = TODO
+        # init vars at beginning of training
+        self.total_envsteps = 0
+        self.start_time = time.time()
 
-            if self.gae_lambda is not None:
-                ## append a dummy T+1 value for simpler recursive calculation
-                values = np.append(values, [0])
+        for itr in range(n_iter):
+            print("\n\n********** Iteration %i ************"%itr)
 
-                ## combine rews_list into a single array
-                rews = np.concatenate(rews_list)
-
-                ## create empty numpy array to populate with GAE advantage
-                ## estimates, with dummy T+1 value for simpler recursive calculation
-                batch_size = obs.shape[0]
-                advantages = np.zeros(batch_size + 1)
-
-                for i in reversed(range(batch_size)):
-                    ## TODO: recursively compute advantage estimates starting from
-                        ## timestep T.
-                    ## HINT 1: use terminals to handle edge cases. terminals[i]
-                        ## is 1 if the state is the last in its trajectory, and
-                        ## 0 otherwise.
-                    ## HINT 2: self.gae_lambda is the lambda value in the
-                        ## GAE formula
-
-                # remove dummy advantage
-                advantages = advantages[:-1]
-
+            # decide if videos should be rendered/logged at this iteration
+            if itr % self.params['video_log_freq'] == 0 and self.params['video_log_freq'] != -1:
+                self.logvideo = True
             else:
-                ## TODO: compute advantage estimates using q_values, and values as baselines
-                advantages = TODO
+                self.logvideo = False
 
-        # Else, just set the advantage to [Q]
-        else:
-            advantages = q_values.copy()
+            # decide if metrics should be logged
+            if self.params['scalar_log_freq'] == -1:
+                self.logmetrics = False
+            elif itr % self.params['scalar_log_freq'] == 0:
+                self.logmetrics = True
+            else:
+                self.logmetrics = False
 
-        # Normalize the resulting advantages
-        if self.standardize_advantages:
-            ## TODO: standardize the advantages to have a mean of zero
-            ## and a standard deviation of one
-            advantages = TODO
+            # collect trajectories, to be used for training
+            training_returns = self.collect_training_trajectories(
+                itr, initial_expertdata, collect_policy, self.params['batch_size']
+            )
+            paths, envsteps_this_batch, train_video_paths = training_returns
+            self.total_envsteps += envsteps_this_batch
 
-        return advantages
+            # add collected data to replay buffer
+            self.agent.add_to_replay_buffer(paths)
 
-    #####################################################
-    #####################################################
+            # train agent (using sampled data from replay buffer)
+            train_logs = self.train_agent()
 
-    def add_to_replay_buffer(self, paths):
-        self.replay_buffer.add_rollouts(paths)
+            # log/save
+            if self.logvideo or self.logmetrics:
+                # perform logging
+                print('\nBeginning logging procedure...')
+                self.perform_logging(itr, paths, eval_policy, train_video_paths, train_logs)
 
-    def sample(self, batch_size):
-        return self.replay_buffer.sample_recent_data(batch_size, concat_rew=False)
+                if self.params['save_params']:
+                    self.agent.save('{}/agent_itr_{}.pt'.format(self.params['logdir'], itr))
 
-    #####################################################
-    ################## HELPER FUNCTIONS #################
-    #####################################################
+    ####################################
+    ####################################
 
-    def _discounted_return(self, rewards):
-        """
-            Helper function
+    def collect_training_trajectories(self, itr, initial_expertdata, collect_policy, batch_size):
+        # TODO: GETTHIS from HW1
+        if itr == 0 and initial_expertdata:
+            with open(initial_expertdata, "rb") as f:
+                loaded_paths = pickle.load(f)
+            return loaded_paths, 0, None
 
-            Input: list of rewards {r_0, r_1, ..., r_t', ... r_T} from a single rollout of length T
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = utils.sample_trajectories(self.env, collect_policy, batch_size, self.params['ep_len'])
 
-            Output: list where each index t contains sum_{t'=0}^T gamma^t' r_{t'}
-        """
+        train_video_paths = None
+        if self.logvideo:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
 
-        # TODO: create list_of_discounted_returns
+        return paths, envsteps_this_batch, train_video_paths
 
-        return list_of_discounted_returns
+    def train_agent(self):
+        # TODO: GETTHIS from HW1
+        print('\nTraining agent using sampled data from replay buffer...')
+        all_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
 
-    def _discounted_cumsum(self, rewards):
-        """
-            Helper function which
-            -takes a list of rewards {r_0, r_1, ..., r_t', ... r_T},
-            -and returns a list where the entry in each index t' is sum_{t'=t}^T gamma^(t'-t) * r_{t'}
-        """
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(self.params['train_batch_size'])
 
-        # TODO: create `list_of_discounted_returns`
-        # HINT: it is possible to write a vectorized solution, but a solution
-            # using a for loop is also fine
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            all_logs.append(train_log)
+        return all_logs
 
-        return list_of_discounted_cumsums
+    ####################################
+    ####################################
+
+    def perform_logging(self, itr, paths, eval_policy, train_video_paths, all_logs):
+
+        last_log = all_logs[-1]
+
+        #######################
+
+        # collect eval trajectories, for logging
+        print("\nCollecting data for eval...")
+        eval_paths, eval_envsteps_this_batch = utils.sample_trajectories(self.env, eval_policy, self.params['eval_batch_size'], self.params['ep_len'])
+
+        # save eval rollouts as videos in tensorboard event file
+        if self.logvideo and train_video_paths != None:
+            print('\nCollecting video rollouts eval')
+            eval_video_paths = utils.sample_n_trajectories(self.env, eval_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+            #save train/eval videos
+            print('\nSaving train rollouts as videos...')
+            self.logger.log_paths_as_videos(train_video_paths, itr, fps=self.fps, max_videos_to_save=MAX_NVIDEO,
+                                            video_title='train_rollouts')
+            self.logger.log_paths_as_videos(eval_video_paths, itr, fps=self.fps,max_videos_to_save=MAX_NVIDEO,
+                                             video_title='eval_rollouts')
+
+        #######################
+
+        # save eval metrics
+        if self.logmetrics:
+            # returns, for logging
+            train_returns = [path["reward"].sum() for path in paths]
+            eval_returns = [eval_path["reward"].sum() for eval_path in eval_paths]
+
+            # episode lengths, for logging
+            train_ep_lens = [len(path["reward"]) for path in paths]
+            eval_ep_lens = [len(eval_path["reward"]) for eval_path in eval_paths]
+
+            # decide what to log
+            logs = OrderedDict()
+            logs["Eval_AverageReturn"] = np.mean(eval_returns)
+            logs["Eval_StdReturn"] = np.std(eval_returns)
+            logs["Eval_MaxReturn"] = np.max(eval_returns)
+            logs["Eval_MinReturn"] = np.min(eval_returns)
+            logs["Eval_AverageEpLen"] = np.mean(eval_ep_lens)
+
+            logs["Train_AverageReturn"] = np.mean(train_returns)
+            logs["Train_StdReturn"] = np.std(train_returns)
+            logs["Train_MaxReturn"] = np.max(train_returns)
+            logs["Train_MinReturn"] = np.min(train_returns)
+            logs["Train_AverageEpLen"] = np.mean(train_ep_lens)
+
+            logs["Train_EnvstepsSoFar"] = self.total_envsteps
+            logs["TimeSinceStart"] = time.time() - self.start_time
+            logs.update(last_log)
+
+            if itr == 0:
+                self.initial_return = np.mean(train_returns)
+            logs["Initial_DataCollection_AverageReturn"] = self.initial_return
+
+            # perform the logging
+            for key, value in logs.items():
+                print('{} : {}'.format(key, value))
+                self.logger.log_scalar(value, key, itr)
+            print('Done logging...\n\n')
+
+            self.logger.flush()
